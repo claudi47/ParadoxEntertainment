c83e552e8136aa12c860f845f3aa0856654d3b98
Valdan17 <valenti.daniele1997@gmail.com>
2022-11-21 21:25:55 +0100

    Scrittura del metodo inserisciPellicola in ParadoxEntertainment

--- src/main/java/com/mycompany/paradoxentertainment/ParadoxEntertainment.java
+++ src/main/java/com/mycompany/paradoxentertainment/ParadoxEntertainment.java
@@ -3,6 +3,8 @@ package com.mycompany.paradoxentertainment;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Map;
+import java.util.TreeMap;
 
 /**
  *
@@ -10,8 +12,10 @@ import java.io.InputStreamReader;
  */
 public class ParadoxEntertainment {
     private static ParadoxEntertainment paradoxEntertainment; //Singleton
-    public static Cinema c;
+    private static Cinema c;
     static BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+    private static TreeMap<Integer, Pellicola> elencoPellicole;
+    private static Pellicola pellicolaCorrente, pellicolaSelezionata;
     
     /**
      * Singleton
@@ -79,9 +83,8 @@ public class ParadoxEntertainment {
                 break;
             
             case 2:
-                System.out.println("Aggiungi Film");
-                System.out.println("Eseguo... \n.\n.\n.");
-                System.out.println("Termino codice addetto, logout...\n\n");
+                inserisciPellicola();
+                menuAmministratore();
                 break;
             
             default:
@@ -95,7 +98,6 @@ public class ParadoxEntertainment {
         int postiStandard;
         int postiVIP;
         int postiTot;
-        boolean conferma;
         
         do {
             System.out.println("Inserisci l'identificativo della sala");
@@ -110,9 +112,6 @@ public class ParadoxEntertainment {
         
         postiTot = postiStandard+postiVIP;
         
-        //Cinema crea l'oggetto Sala e lo restituisce a Paradox
-        c.inserisciSala(nomeSala, postiStandard, postiVIP);
-        
         System.out.println("\nRiepilogo: \n"
                     + " - Nome Sala: " + nomeSala + "\n"
                     + " - Posti Standard: " + postiStandard + "\n"
@@ -126,7 +125,10 @@ public class ParadoxEntertainment {
            return;
         } 
         
-        //Funzione di conferma dell'aggiunta della sala all'elenco Sale del cinema
+        //Viene creato l'oggetto Sala
+        c.inserisciSala(nomeSala, postiStandard, postiVIP);
+        
+        //Viene aggiunto all'elenco delle sale del cinema
         confermaSala();
         
         System.out.println("Inserimento confermato!");
@@ -135,6 +137,71 @@ public class ParadoxEntertainment {
     
     public static void confermaSala() {
         c.confermaSala();
+    }
+    
+    public static void inserisciPellicola() throws IOException {
+        String nomePellicola;
+        String regista;
+        int anno;
+        String genere;
+        int durata;
+        int idPellicola;
+        
+        do {
+            System.out.println("Inserisci il titolo della pellicola");
+            nomePellicola = bf.readLine();
+            
+            System.out.println("Inserisci il nome del regista");
+            regista = bf.readLine();
+        
+            System.out.println("Inserisci l'anno di uscita");
+            anno = Integer.parseInt(bf.readLine());
+        } while((verificaPellicola(nomePellicola, regista, anno) == true));
+        
+        System.out.println("Inserisci il genere della pellicola");
+        genere = bf.readLine();
+        
+        System.out.println("Inserisci la durata in minuti");
+        durata = Integer.parseInt(bf.readLine());
+        
+        System.out.println("\nRiepilogo: \n"
+                    + " - Titolo: " + nomePellicola + "\n"
+                    + " - Regista: " + regista + "\n"
+                    + " - Anno: " + anno + "\n"
+                    + " - Genere: " + genere + "\n"
+                    + " - Durata: " + durata + "\n"
+                    + "Premere 1 per confermare, 0 per annullare l'inserimento"
+                    );
+        
+        if(Integer.parseInt(bf.readLine()) == 0) {
+            System.out.println("Inserimento annullato\n");
+            return;
+        }
+        
+        // creazione oggetto Pellicola
+        pellicolaCorrente = new Pellicola(nomePellicola, regista, anno, genere, durata);
+        
+        // aggiunta all'elenco delle pellicole
+        pellicolaCorrente.setIdPellicola(elencoPellicole.firstKey());
+        
+        /* verifica come funzionano le TreeMap:
+        TreeMaps don't work that way. They're not an indexed collection (unlike Lists), so what you're trying to do just won't work.
+        You need to remove the old one, if the key changes, and reinsert the new key-value pair.
+        
+        forse meglio una List in cui si ha l'oggetto Pellicola col suo ID che viene aggiunto in elencoPellicole[ID]?
+        */
+        elencoPellicole.
+        
+    }
+    
+    public static boolean verificaPellicola(String nomePellicola, String regista, int anno) {
+        for (Map.Entry<Integer, Pellicola> entry : elencoPellicole.entrySet()) {
+            if(entry.getValue().getNomePellicola().equals(nomePellicola) &&
+                    entry.getValue().getRegista().equals(regista) &&
+                        entry.getValue().getAnno() == anno) 
+                return true;         
+        } 
+        return false;
     }
             
     public static void main(String[] args) throws IOException {
